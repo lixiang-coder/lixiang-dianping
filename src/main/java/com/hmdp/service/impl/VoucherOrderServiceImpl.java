5835f15142aea9272c7eb5a88f26adddb171d029
xzy <2894702202@qq.com>
2024-04-18 16:14:45 +0800

    day03--乐观锁解决超卖问题业务代码

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -51,9 +51,10 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
         // 5.扣减库存
         boolean success = seckillVoucherService.update().
-                setSql("stock = stock - 1").
-                eq("voucher_id", voucherId).update();
-        if (!success){
+                setSql("stock = stock - 1").    // set stock = stock - 1
+                eq("voucher_id", voucherId).gt("stock", 0). //where voucher_id = ? and stock > 0
+                update();
+        if (!success) {
             //扣减库存失败
             return Result.fail("库存不足");
         }
