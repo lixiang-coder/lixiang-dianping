3f464ed0b034746f7e5b40318e32410fa0835cef
xzy <2894702202@qq.com>
2024-04-19 14:24:49 +0800

    day04--解决分布式锁的原子性问题（调用Lua脚本）

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,12 @@
 package com.hmdp.utils;
 
-import cn.hutool.core.lang.UUID;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
+import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
@@ -13,6 +17,13 @@ public class SimpleRedisLock implements ILock {
     private static final String KEY_PREFIX = "lock";
 
     private StringRedisTemplate stringRedisTemplate;
+
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -32,6 +43,15 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unlock() {
+        // 调用lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
+    }
+
+    /*@Override
+    public void unlock() {
         //获取线程中的标识
         String threadId = ID_PREFIX + Thread.currentThread().getId();
 
@@ -43,5 +63,5 @@ public class SimpleRedisLock implements ILock {
             // 释放锁
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
-    }
+    }*/
 }
